1a69ea0ecc273b0d87a396f217f7cee5ecf894ba
Bipin Thapa <yukichin638@gmail.com>
2025-08-11 22:12:46 +0545

    Projects link added

--- src/app/portfolio/portfolioContainer.js
+++ src/app/portfolio/portfolioContainer.js
@@ -2,8 +2,16 @@ import "../global.css";
 import "./portfolio.css";
 
 export default function PortfolioContainer(){
+  const projects = {
+    hackfest: "https://kist-hackfest2025.vercel.app/",
+    birthcalc: "https://birthcalcz.netlify.app/",
+    note: "https://mero-note.netlify.app/",
+    passGen: "https://bipin-thapa01.github.io/Password-Generator/",
+    memoryGame: "https://bipin-thapa01.github.io/Memory-Game/index.html",
+    kistWebsite: "https://bipin-thapa01.github.io/Memory-Game/index.html",
+    textEditor: "https://github.com/bipin-thapa01/Python",
+  }
 
-  
   return(
     <div className="main-container">
       <h3>
