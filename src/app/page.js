b2e24dcac0d69c6fe66a7ae72b4120a12246eacf
Bipin Thapa <yukichin638@gmail.com>
2025-08-16 21:14:40 +0545

    Loading Page Added

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,24 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import HomePage from "./HomePage";
 import Nav from "./nav";
 import Footer from "./footer";
 import "./global.css";
 
 export default function Page(){
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(()=>{
+    setIsLoaded(true);
+  },[])
+
+  if(!isLoaded){
+    return <div className="loading-bg">
+      Loading. Be Patient...
+    </div>
+  }
+
   return(
     <div className="main-bg">
       <Nav/>
