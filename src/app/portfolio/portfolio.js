4597122ca3d9bfed58b6ca0e22e15cafea021d70
Bipin Thapa <yukichin638@gmail.com>
2025-08-16 21:17:25 +0545

    Loading Page Added to all pages

--- src/app/portfolio/portfolio.js
+++ src/app/portfolio/portfolio.js
@@ -1,11 +1,24 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import PortfolioContainer from "./portfolioContainer";
 import Nav from "../nav";
 import Footer from "../footer";
 import "../global.css";
 
 export default function Portfolio(){
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(()=>{
+    setIsLoaded(true);
+  },[])
+
+  if(!isLoaded){
+    return <div className="loading-bg">
+      Loading. Be Patient...
+    </div>
+  }
+
   return(
     <div className="main-bg">
       <Nav/>
--- src/app/resume/resume.js
+++ src/app/resume/resume.js
@@ -1,11 +1,24 @@
 "use client";
 
+import {useState, useEffect} from "react";
 import Nav from "../nav";
 import ResumeContainer from "./ResumeContainer";
 import Footer from "../footer";
 import "../global.css";
 
 export default function Resume() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(()=>{
+    setIsLoaded(true);
+  },[])
+
+  if(!isLoaded){
+    return <div className="loading-bg">
+      Loading. Be Patient...
+    </div>
+  }
+
   return (
     <>
       <Nav />
